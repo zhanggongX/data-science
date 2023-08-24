dda215484ae4894c1179cc20470e2a32ad679e3f
zhanggong <zhanggong@58.com>
2023-08-24 16:43:28 +0800

    pandas exercise

---
+++ 570.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+
+# select name
+# from Employee e
+#  where id in
+#     (select managerId
+#         from Employee group by managerId
+#         having count(managerId) >= 5);
+def find_managers(employee: pd.DataFrame) -> pd.DataFrame:
+    df = employee.groupby('managerId').size().reset_index(name='manageCount')
+    df = df[df['manageCount'] >= 5]
+    print(df)
+    df = employee.merge(df, left_on='id', right_on='managerId', how='left')
+    df = df[~df['manageCount'].isnull()][['name']]
+    return df
+
+
+if __name__ == '__main__':
+    data = [[101, 'John', 'A', None],
+            [102, 'Dan', 'A', 101],
+            [103, 'James', 'A', 101],
+            [104, 'Amy', 'A', 101],
+            [105, 'Anne', 'A', 101],
+            [106, 'Ron', 'B', 101]]
+    Employee = (pd.DataFrame(data, columns=['id', 'name', 'department', 'managerId'])
+                .astype({'id': 'Int64', 'name': 'object', 'department': 'object', 'managerId': 'Int64'}))
+    res = find_managers(Employee)
+    print(res)
