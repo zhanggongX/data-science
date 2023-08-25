258a6765d3145d0fcaa3fa0c88e9818b1acb4a4e
zhanggong <zhanggong@58.com>
2023-08-25 16:06:43 +0800

    pandas exercise

--- 1907.py
+++ 1907.py
@@ -2,21 +2,34 @@ import pandas as pd
 
 
 def count_salary_categories(accounts: pd.DataFrame) -> pd.DataFrame:
-    for x in accounts.index:
-        if accounts.loc[x, 'income'] < 20000:
-            accounts.loc[x, 'category'] = 'Low Salary'
-        elif accounts.loc[x, 'income'] > 50000:
-            accounts.loc[x, 'category'] = 'Average Salary'
-        else:
-            accounts.loc[x, 'category'] = 'High Salary'
-    df = accounts.groupby(['category']).agg({'account_id': 'count'}).reset_index()
-    df.rename(columns={'account_id': 'accounts_count'}, inplace=True)
+    # for x in accounts.index:
+    #     if accounts.loc[x, 'income'] < 20000:
+    #         accounts.loc[x, 'category'] = 'Low Salary'
+    #     elif accounts.loc[x, 'income'] > 50000:
+    #         accounts.loc[x, 'category'] = 'High Salary'
+    #     else:
+    #         accounts.loc[x, 'category'] = 'Average Salary'
+    # df = accounts.groupby(['category']).agg({'account_id': 'count'}).reset_index()
+    # df.rename(columns={'account_id': 'accounts_count'}, inplace=True)
+    # print(df)
+    #
+    # df_category = pd.DataFrame([['Low Salary'], ['Average Salary'], ['High Salary']], columns=['category'])
+    # df = pd.merge(df_category, df, on='category', how='left')
+    # df.fillna({'accounts_count': 0}, inplace=True)
+    # df = df.astype({'accounts_count': 'int64'})
+    # print(df)
+    # return df
 
-    df_category = pd.DataFrame([['Low Salary'], ['Average Salary'], ['High Salary']], columns=['category'])
-    df = pd.merge(df_category, df, on='category', how='left')
-    df.fillna({'accounts_count': 0}, inplace=True)
-    df = df.astype({'accounts_count': 'int64'})
-    print(df)
+    low_count = (accounts['income'] < 20000).sum()
+    average_count = ((accounts['income'] >= 20000) & (accounts['income'] <= 50000)).sum()
+    high_count = (accounts['income'] > 50000).sum()
+
+    ans = pd.DataFrame({
+        'category': ['Low Salary', 'Average Salary', 'High Salary'],
+        'accounts_count': [low_count, average_count, high_count]
+    })
+
+    return ans
 
 
 if __name__ == '__main__':
