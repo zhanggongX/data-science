6d1d3c54f7768384832e727c96d6b25e6e79680d
zhanggong <zhanggong@58.com>
2023-08-28 11:49:01 +0800

    pandas exercise

--- base/1768.py
+++ base/1768.py
@@ -1,17 +1,16 @@
-class Solution:
-    def mergeAlternately(self, word1: str, word2: str) -> str:
-        i, j, x, y = 0, 0, len(word1), len(word2)
-        res = ''
-        while i < x or j < y:
-            if i < x:
-                res += word1[i]
-                i += 1
-            if j < y:
-                res += word2[j]
-                j += 1
-        return res
+def merge_alternate(word1: str, word2: str) -> str:
+    i, j, x, y = 0, 0, len(word1), len(word2)
+    res = ''
+    while i < x or j < y:
+        if i < x:
+            res += word1[i]
+            i += 1
+        if j < y:
+            res += word2[j]
+            j += 1
+    return res
 
 
 if __name__ == '__main__':
-    result = Solution.mergeAlternately(self='1768,'abc', 'pqr')
+    result = merge_alternate('abc', 'pqr')
     print(result)
---
+++ base/389.py
@@ -0,0 +1,9 @@
+def find_the_difference(s: str, t: str) -> str:
+    for i in t:
+        if t.count(i) - s.count(i) == 1:
+            return i
+
+
+if __name__ == '__main__':
+    result = find_the_difference('abc', 'abcd')
+    print(result)
